b2570f6bdaf671fb00c6e235ace5e279a5554906
Jos Lagerweij <jos@lagerwey.net>
2023-02-10 13:52:27 +0100

    Unit tests for Clean Code session

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -3,8 +3,12 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    public GildedRose(Item... items) {
         this.items = items;
+    }
+
+    public Item[] getItems() {
+        return items;
     }
 
     public void updateQuality() {
---
+++ Java/src/test/java/com/gildedrose/ItemTestHelper.java
@@ -0,0 +1,15 @@
+package com.gildedrose;
+
+import com.gildedrose.Item;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ItemTestHelper {
+
+    public static void assertItemEquals(Item actual, Item expected) {
+        assertEquals(expected.name, actual.name);
+        assertEquals(expected.quality, actual.quality);
+        assertEquals(expected.sellIn, actual.sellIn);
+    }
+}
